63d45a57f1f7fdae69468d01ad0f0125ba94f417
YukiYoshizawa <eatermgsorigin4@gmail.com>
2024-11-12 05:27:45 +0000

    [add]投稿機能を追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,8 +1,21 @@
 class BooksController < ApplicationController
   def new
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new(book_params)
+    if @book.save
+      flash[:notice] = "successfully"
+      redirect_to book_path(@book.id)
+    else
+      flash.now[:alert] = "failed"
+      render :new
+    end
   end
 
   def index
+    @books = Book.all
   end
 
   def show
@@ -10,4 +23,9 @@ class BooksController < ApplicationController
 
   def edit
   end
+
+  private
+    def book_params
+      params.require(:book).permit(:title, :body)
+    end
 end
